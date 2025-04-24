80c3b6d6eb0bc8c6b4deed8d5752a30d1ce5d202
nitasha18 <nitashag18@gmail.com>
2025-04-24 22:44:47 +0530

    Fix Vercel routing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is set
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'src/main.jsx'
-    }
+    emptyOutDir: true
   }
-})
+});
